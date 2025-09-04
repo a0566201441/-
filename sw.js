84b1d6cb4ed5358e3e3c3a3ccc479a14232d9f8b
a0566201441 <a0566201441@gmail.com>
2025-09-04 06:05:17 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "budget-pwa-v35";
+const CACHE = "budget-pwa-v36";
 const CORE_ASSETS = ["./","./index.html","./manifest.webmanifest","./icons/pwa-192.png","./icons/pwa-512.png"];
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE_ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
