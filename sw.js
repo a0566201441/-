8e91323779a0c415c84364f01371295427046822
a0566201441 <a0566201441@gmail.com>
2025-09-04 04:09:59 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,16 @@
 
-const CACHE = "budget-pwa-v19";
-const CORE_ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./icons/pwa-192.png",
-  "./icons/pwa-512.png"
-];
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE_ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-self.addEventListener("fetch", (event) => {
-  const req = event.request;
-  const url = new URL(req.url);
+const CACHE = "budget-pwa-v20";
+const CORE_ASSETS = ["./","./index.html","./manifest.webmanifest","./icons/pwa-192.png","./icons/pwa-512.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE_ASSETS))); self.skipWaiting(); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener("fetch", e => {
+  const req = e.request; const url = new URL(req.url);
   if (url.origin === location.origin) {
-    event.respondWith(
-      caches.match(req).then((cached) => {
-        const fetchAndUpdate = fetch(req).then((res) => {
-          const copy = res.clone();
-          caches.open(CACHE).then((c) => c.put(req, copy));
-          return res;
-        }).catch(() => cached);
-        return cached || fetchAndUpdate;
-      })
-    );
+    e.respondWith(caches.match(req).then(cached => {
+      const fetchAndUpdate = fetch(req).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(req, copy)); return res; }).catch(() => cached);
+      return cached || fetchAndUpdate;
+    }));
   } else {
-    event.respondWith(
-      fetch(req).then((res) => {
-        const copy = res.clone();
-        caches.open(CACHE).then((c) => c.put(req, copy));
-        return res;
-      }).catch(() => caches.match(req))
-    );
+    e.respondWith(fetch(req).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(req, copy)); return res; }).catch(() => caches.match(req)));
   }
 });
